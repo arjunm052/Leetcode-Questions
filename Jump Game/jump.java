23f963c76e27ed9c3cb1d26bf628a0a45e228732
arjunm052 <singh.arjun127@gmail.com>
2021-10-21 13:09:08 +0530

    Jump Game II

--- Jump Game/jump.java
+++ Jump Game/jump.java
@@ -8,6 +8,7 @@ public class jump {
         System.out.println(reachable(arr));
     }
 
+    // Return boolean value on whether you can reach the end or not
     public static boolean reachable(int[] arr) {
         // max represents the farthest index reachable till now
         int max = 0;
@@ -20,5 +21,28 @@ public class jump {
         }
 
         return true;
+    }
+
+    // Find the minimum number of jumps to reach end
+    public static int jump(int[] nums) {
+
+        // Initialize the dp array with Integer MAX_VALUE
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        // Minimum values to reach index 0 is 0
+        dp[0] = 0;
+        // Start filling up the dp array
+        for (int i = 1; i < nums.length; i++) {
+            // Traverse from 0 to i-1
+            for (int j = 0; j < i; j++) {
+                // and check if ith index is reachable from jth index, if yes then update the
+                // value
+                if (nums[j] + j >= i) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        // return the final answer
+        return dp[nums.length - 1];
     }
 }
